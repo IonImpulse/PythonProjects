bb9a8c6b0689a2aebcc2d09dfc4c7721fd48da0a
Ethan <edv121@outlook.com>
2019-05-17 13:24:52 -0700

    updated engine for file to sarcasm

--- Random Projects/sarcasmFromTxt.py
+++ Random Projects/sarcasmFromTxt.py
@@ -1,11 +1,14 @@
 file = input("Enter full file name: ")
 file = open(file, "r")
 lists = list(file.read())
+loopIndex = 0
 for x, y in enumerate(lists) :
-    if x % 2 == 0 :
-        lists[x] = y.upper()
-    else :
-        lists[x] = y.lower()
+    if y != " " :
+        if loopIndex % 2 == 0 :
+            lists[x] = y.upper()
+        else :
+            lists[x] = y.lower()
+        loopIndex += 1
 print("".join(lists))
 saveName = input("Enter save name: ")
 file = open(saveName, "w")
