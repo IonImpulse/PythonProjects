b7e7fc1b5c1345ca92e68ab045cbc9dccacbed76
Ethan <edv121@outlook.com>
2019-05-17 12:59:31 -0700

    added loop for sarcasm

--- Random Projects/sarcasm.py
+++ Random Projects/sarcasm.py
@@ -1,9 +1,11 @@
 import random
-sentence = input("Type in sentence: ")
-lists = list(sentence)
-for x, y in enumerate(lists) :
-    if x % 2 == 0 :
-        lists[x] = y.upper()
-    else :
-        lists[x] = y.lower()
-print("".join(lists))
+sentence = ""
+while sentence.lower() != "exit" :
+    sentence = input("Type in sentence, \"exit\" to close: ")
+    lists = list(sentence)
+    for x, y in enumerate(lists) :
+        if x % 2 == 0 :
+            lists[x] = y.upper()
+        else :
+            lists[x] = y.lower()
+    print("".join(lists))
