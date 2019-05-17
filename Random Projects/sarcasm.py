c5946aa0d819bb910e8daf2181ae57e70eafb03b
Ethan <edv121@outlook.com>
2019-05-17 13:14:48 -0700

    made better

--- Random Projects/sarcasm.py
+++ Random Projects/sarcasm.py
@@ -1,11 +1,15 @@
 import random
 sentence = ""
+print("Type in sentence, \"exit\" to close: ")
 while sentence.lower() != "exit" :
-    sentence = input("Type in sentence, \"exit\" to close: ")
+    sentence = input(":")
     lists = list(sentence)
+    loopIndex = 0
     for x, y in enumerate(lists) :
-        if x % 2 == 0 :
-            lists[x] = y.upper()
-        else :
-            lists[x] = y.lower()
+        if y != " " :
+            if loopIndex % 2 == 0 :
+                lists[x] = y.upper()
+            else :
+                lists[x] = y.lower()
+            loopIndex += 1
     print("".join(lists))
