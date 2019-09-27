6342a88610b86e955332e578f93c5ae9182285c5
Ethan <edv121@outlook.com>
2019-09-27 14:16:07 -0700

    finished p + q

--- NumworksCalc/QandPAddition/pandqaddition.py
+++ NumworksCalc/QandPAddition/pandqaddition.py
@@ -5,7 +5,7 @@ linFunction = []
 
 print("======P + Q Solver======")
 print("Step 1: Cubic Function")
-print("ax^3 + ax + b")
+print("x^3 + ax + b")
 print("a?")
 cubicFunction.append(float(input(":")))
 print("b?")
@@ -23,3 +23,12 @@ linFunction.append(float(input(":")))
 
 linSlope = (linFunction[0]-linFunction[2])/(linFunction[1]-linFunction[3])
 linIntercept = linFunction[1] - (linSlope * linFunction[0])
+
+#Xr here
+linFunction.append(linSlope - linFunction[0] - linFunction[2])
+
+#Xy here
+linFunction.append((linSlope * linFunction[4]) + linIntercept)
+
+print("\nP + Q:")
+print("X = " + str(linFunction[4]) + "\nY = " + str(0 - linFunction[5]))
