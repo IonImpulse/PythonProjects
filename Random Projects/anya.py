6b935c1b514ccc1659b1911338f67262d1d3bf3e
Ethan <edv121@outlook.com>
2020-03-02 16:46:55 -0800

    done ?

--- Random Projects/anya.py
+++ Random Projects/anya.py
@@ -1,21 +1,51 @@
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 import csv
+import tkinter as tk
+from tkinter import filedialog
+from time import sleep
 
-geolocator = Nominatim(user_agent="edv121@outlook.com", timeout=10)
+def getLocation(inLocation, timeoutNum = 10) :
+    if timeoutNum != 0 :
+        try:
+            location = geolocator.geocode(inLocation)
+        except Exception as e:
+            location = getLocation(inLocation, timeoutNum - 1)
+    else :
+        print("Timeout, waiting 1 minute")
+        sleep(60)
+        location = getLocation(inLocation)
+    return location
 
-location = geolocator.geocode("160 John St, Seattle, WA 98109")
+root = tk.Tk()
+root.withdraw()
+
+geolocator = Nominatim(user_agent="edv121@outlook.com", timeout=5)
+
+location = getLocation("160 John St, Seattle, WA 98109")
 The_Downtown_School_A_Lakeside_School = (location.latitude, location.longitude)
-print("location")
-with open("C:\\Users\\ionim\\Documents\\GitHub\\PythonProjects\\Random Projects\\InstitutionCampus.csv", newline = "") as file :
+
+inputFile = filedialog.askopenfilename(filetypes = (("Comma Seperated Values","*.csv"),("All files", "*.*")))
+
+with open("inputFile", newline = "") as file :
     inputDataRaw = [row for row in csv.reader(file)]
 
 total = len(inputDataRaw)
+print("Grabbed file with " + str(total) + " colleges. Starting geopy requests...")
+
 outputTable = [["Name","Address","Distance from DTS"]]
 
 for index, school in enumerate(inputDataRaw) :
+    if index % 100 == 0 :
+        with open("OutputDistances.csv", "w", newline = "") as file :
+            csv_writer = csv.writer(file, dialect='excel')
+            for i in outputTable:
+                csv_writer.writerow(i)    
+            outputTable = []
+    
     if school[2] == "Institution" :
-        location = geolocator.geocode(school[1])
+        location = getLocation(school[1])
+        
         if location != None :
             location = (location.latitude, location.longitude)
             distance = geodesic(The_Downtown_School_A_Lakeside_School, location).miles
@@ -26,7 +56,7 @@ for index, school in enumerate(inputDataRaw) :
             print("Skipping " + school[0])
             print("")
 
-with open("C:\\Users\\ionim\\Documents\\GitHub\\PythonProjects\\Random Projects\\OutputDistances.csv", "w", newline = "") as file :
+with open("OutputDistances.csv", "w", newline = "") as file :
     csv_writer = csv.writer(file, dialect='excel')
     for i in outputTable:
         csv_writer.writerow(i)
