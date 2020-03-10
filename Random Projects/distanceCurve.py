4143741b8d668254a22f8318cb53b17f0026e1e6
Ethan <edv121@outlook.com>
2020-03-10 10:46:14 -0700

    distance things

--- Random Projects/distanceCurve.py
+++ Random Projects/distanceCurve.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import filedialog
 import csv
 
-def getDistance(origin, destination):
+def getDistanceReal(origin, destination):
     """
     Calculate the Haversine distance.
 
@@ -39,6 +39,36 @@ def getDistance(origin, destination):
 
     return d
 
+def getDistanceBad(origin, destination) :
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+
+    d = math.sqrt(((lat2-lat1)**2) + ((lon2-lon1)**2))
+
+    return d
+
+def angleFromCoordinateGood(lat1, long1, lat2, long2):
+    dLon = (long2 - long1)
+
+    y = math.sin(dLon) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dLon)
+
+    brng = math.atan2(y, x)
+
+    brng = math.degrees(brng)
+    brng = (brng + 360) % 360
+    brng = 360 - brng # count degrees clockwise - remove to make counter-clockwise
+
+    return brng
+
+def angleFromCoordinate(lat1, long1, lat2, long2) :
+    x = lat2 - lat1
+    y = long2 - long1
+
+    angle = (math.atan2(y,x))
+
+    return angle
+
 root = tk.Tk()
 root.withdraw()
 
@@ -48,7 +78,20 @@ with open(inputFile, newline = "") as file :
     inputDataRaw = [row for row in csv.reader(file)]
 
 total = 0
+angle = False
 
+direction = angleFromCoordinate(float(inputDataRaw[0][0]),float(inputDataRaw[0][1]), float(inputDataRaw[1][0]), float(inputDataRaw[1][1]))
 for i in range(len(inputDataRaw) - 1) :
-    total += getDistance((float(inputDataRaw[i][0]),float(inputDataRaw[i][1])), (float(inputDataRaw[i + 1][0]), float(inputDataRaw[i + 1][1])))
-    print(total)
+    total += getDistanceBad((float(inputDataRaw[i][0]),float(inputDataRaw[i][1])), (float(inputDataRaw[i + 1][0]), float(inputDataRaw[i + 1][1])))
+    angle += abs(angleFromCoordinate(float(inputDataRaw[i][0]),float(inputDataRaw[i][1]), float(inputDataRaw[i + 1][0]), float(inputDataRaw[i + 1][1])))
+
+print("Total Curve: " + str(angle) + " Degrees")
+print("Average Curve: " + str(angle/total) + " Degrees")
+crow = getDistanceBad((float(inputDataRaw[0][0]),float(inputDataRaw[0][1])), (float(inputDataRaw[-1][0]),float(inputDataRaw[-1][1])))
+print("Length: " + str(total))
+print("Crow Flys: " + str(crow))
+try :
+    print("Ratio " + str(crow/total))
+except Exception as e :
+    print("Ratio Division by Zero")
+
