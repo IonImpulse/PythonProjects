3ca0c3158a0aae57027585f33923fe94fbc41ba7
Ethan Vazquez <edv121@outlook.com>
2020-03-23 10:43:37 -0700

    cleaned up

--- Random Projects/distanceCurve.py
+++ Random Projects/distanceCurve.py
@@ -3,42 +3,6 @@ import tkinter as tk
 from tkinter import filedialog
 import csv
 
-def getDistanceReal(origin, destination):
-    """
-    Calculate the Haversine distance.
-
-    Parameters
-    ----------
-    origin : tuple of float
-        (lat, long)
-    destination : tuple of float
-        (lat, long)
-
-    Returns
-    -------
-    distance_in_feet : float
-
-    Examples
-    --------
-    >>> origin = (48.1372, 11.5756)  # Munich
-    >>> destination = (52.5186, 13.4083)  # Berlin
-    >>> round(distance(origin, destination), 1)
-    504.2
-    """
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    radius = 6371  # km
-
-    dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lon2 - lon1)
-    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
-         math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
-         math.sin(dlon / 2) * math.sin(dlon / 2))
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    d = radius * c * 0.6213712 * 5280
-
-    return d
-
 def getDistanceBad(origin, destination) :
     lat1, lon1 = origin
     lat2, lon2 = destination
@@ -46,20 +10,6 @@ def getDistanceBad(origin, destination) :
     d = math.sqrt(((lat2-lat1)**2) + ((lon2-lon1)**2))
 
     return d
-
-def angleFromCoordinateGood(lat1, long1, lat2, long2):
-    dLon = (long2 - long1)
-
-    y = math.sin(dLon) * math.cos(lat2)
-    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dLon)
-
-    brng = math.atan2(y, x)
-
-    brng = math.degrees(brng)
-    brng = (brng + 360) % 360
-    brng = 360 - brng # count degrees clockwise - remove to make counter-clockwise
-
-    return brng
 
 def angleFromCoordinate(lat1, long1, lat2, long2) :
     x = lat2 - lat1
